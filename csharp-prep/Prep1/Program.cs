921ef1535516c69efaa378be45fe84c57b4d994f
Trevor Smith <Trev43@byui.edu>
2026-04-22 16:11:07 -0600

    Now asked for first and last name formatting accordingly

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name?");
+        string firstName;
+        firstName = Console.ReadLine();
+
+        Console.Write("What is your Last name?");
+        string lastname = Console.ReadLine();
+
+        Console.WriteLine($"{lastname}, {firstName} {lastname}");
+
+        int x =10;
+
+        if(x == 10)
+        {
+           Console.WriteLine("10");  
+        }
+
+        else if (x == 20)
+        {
+            Console.WriteLine("bwtyer");
+        }
+        else
+        {
+            
+        }
+
     }
 }
