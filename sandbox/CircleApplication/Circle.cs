0c52f0fa1905cde434650127325e5463b7f13193
Trevor Smith <Trev43@byui.edu>
2026-05-04 14:47:08 -0600

    Created CircleApplication and added to nots

---
+++ sandbox/CircleApplication/Circle.cs
@@ -0,0 +1,16 @@
+class Circle
+{
+    double _radius;
+
+
+    public void SetRadius(double radius)
+    {
+        _radius = radius;
+    }
+
+    public double GetArea()
+    {
+        return Math.PI * _radius *_radius;
+    }
+
+}
---
+++ sandbox/CircleApplication/Program.cs
@@ -0,0 +1,9 @@
+﻿class Program
+{
+    public static void Main(string[]args)
+    {
+        Circle myCircle = new Circle();
+        myCircle.SetRadius(10);
+        Console.WriteLine(myCircle.GetArea());
+    }
+}
