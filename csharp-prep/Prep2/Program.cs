354b52cae9665405e2bdd8156adde912022ff16f
Trevor Smith <Trev43@byui.edu>
2026-05-06 13:32:31 -0600

    Prep 2 if else statements

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What was your grade percentage? ");
+        Console.ReadLine
+
+        if (x >= 90)
+        {
+            Console.WriteLine("You recieved an A");
+        }
+
+        else (x >= 80 <90)
+        {
+            Console.WriteLine("You recieved an B");
+        }
+
+        else if (x >= 70 < 80)
+        {
+            Console.WriteLine("You recieved a C");
+        }
+
+        else (x < 70)
+        {
+            Console.WriteLine("Unfortunately you shall not pass! ...Feel free to try again");
+        }
     }
 }
