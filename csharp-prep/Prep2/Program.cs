2483bce798f64eb39f55452451ba77f267535394
Trevor Smith <Trev43@byui.edu>
2026-05-06 13:43:33 -0600

    Prep 2 call functions

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,26 +5,41 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("What was your grade percentage? ");
-        Console.ReadLine
+        string x = Console.ReadLine();
+        int percent = int.Parse(x);
 
         if (x >= 90)
         {
             Console.WriteLine("You recieved an A");
         }
 
-        else (x >= 80 <90)
+        else (x >= 80)
         {
             Console.WriteLine("You recieved an B");
         }
 
-        else if (x >= 70 < 80)
+        else if (x >= 70)
         {
             Console.WriteLine("You recieved a C");
         }
 
+        else if (x >= 60)
+        {
+            Console.WriteLine("You received a D");
+        }
+
+        else if (x < 60)
+        {
+            Console.WriteLine("You recieved an F");
+        }
+    }
+
+    Pass(x,)
+    {
         else (x < 70)
         {
             Console.WriteLine("Unfortunately you shall not pass! ...Feel free to try again");
         }
     }
+    
 }
