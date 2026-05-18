4d81065e91aaa4e267cda0f89256c103f4c32ce4
Trevor Smith <Trev43@byui.edu>
2026-05-18 16:19:24 -0600

    Angle Program

---
+++ sandbox/Angle/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    public static void Main(string[]args)
+    {
+        Console.WriteLine("Hello World");
+
+        Angle myAngle = new Angle();
+        myAngle.SetRadians(10);
+        Console.WriteLine(myAngle.GetRadians());
+    }
+}
---
+++ sandbox/Angle/angle.cs
@@ -0,0 +1,15 @@
+class Angle
+{
+    private double radians;
+
+    public double GetRadians()
+    {
+        return _radians;
+    }
+
+    public void SetRadians(double radians)
+    {
+        _radians = radians;
+    }
+    
+}
