6c4a8ab103dfcf553f338a8c86a39a8d160ef412
Trevor Smith <Trev43@byui.edu>
2026-05-31 17:39:39 -0600

    Prep 4 Complete

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
+        string number = Console.ReadLine();
+        int numero = int.Parse(number);
+        List<int> numeros = new List<int>();
+
+        while (numero != 0)
+        {
+            numeros.Add(numero);                    //Capitalize Add
+            Console.Write("");
+            number = Console.ReadLine();
+            numero = int.Parse(number);
+           
+            
+        }
+        int sum = numeros.Sum();
+        Console.WriteLine($"The Sum is: {sum}");
+
+        double average = numeros.Average();
+        Console.WriteLine($"The average is: {average}");
+
+        int largestNumber = numeros.Max();
+        Console.WriteLine($"The largest number is: {largestNumber}");
     }
 }
