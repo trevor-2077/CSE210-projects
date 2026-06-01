f161ed5850a2727ab2558df431cfa4dcdc21ab38
Trevor Smith <Trev43@byui.edu>
2026-06-01 16:35:56 -0600

    Race times completed

---
+++ sandbox/RaceTimes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<double> times = new List<double> {9.58, 9.69, 9.72, 9.74, 9.78 }; 
+        double smallest = GetSmallestNumber(times);
+        Console.WriteLine($"Fastest time: {smallest}");
+    }
+
+    static double GetSmallestNumber(List<double> times)
+    {
+        double smallest = times[0];
+
+        for (int i =1; i < times.Count; i++)
+        {
+            if (times[i] < smallest)
+            {
+                smallest = times[i];
+            }
+        }
+        return smallest;
+    }
+}
